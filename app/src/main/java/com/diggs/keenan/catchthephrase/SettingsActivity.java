c23b4148193c5667c605a4fea461f61bc9a21c65
keenan <keenan@musistic.com>
2016-12-08 21:12:45 -0600

    disallow fullscreen on How To Play & Settings, change labels for these activities

--- app/src/main/java/com/diggs/keenan/catchthephrase/SettingsActivity.java
+++ app/src/main/java/com/diggs/keenan/catchthephrase/SettingsActivity.java
@@ -3,18 +3,30 @@ package com.diggs.keenan.catchthephrase;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
 
+    CheckBox people;
+    CheckBox food;
+    CheckBox animals;
+    CheckBox household;
+    CheckBox words;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        parameterizeCheckBoxes();
+        setBoxPreferences();
     }
 
     public void onCheckboxClicked(View view) {
@@ -25,7 +37,7 @@ public class SettingsActivity extends AppCompatActivity {
         boolean checked = ((CheckBox) view).isChecked();
 
         // determine which box was selected
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.checkbox_people:
                 if (checked)
                     editor.putBoolean("sublist_people", true).apply();
@@ -57,5 +69,32 @@ public class SettingsActivity extends AppCompatActivity {
                     editor.putBoolean("sublist_words", false).apply();
                 break;
         }
+        Map<String, ?> keys = preferences.getAll();
+        for (Map.Entry<String, ?> entry : keys.entrySet()) {
+            Log.d("map values", entry.getKey() + ": " + entry.getValue().toString());
+        }
+    }
+
+    private void parameterizeCheckBoxes() {
+        people = (CheckBox) findViewById(R.id.checkbox_people);
+        food = (CheckBox) findViewById(R.id.checkbox_food);
+        animals = (CheckBox) findViewById(R.id.checkbox_animals);
+        household = (CheckBox) findViewById(R.id.checkbox_household);
+        words = (CheckBox) findViewById(R.id.checkbox_words);
+    }
+
+    private void setBoxPreferences() {
+        preferences = getPreferences(MODE_PRIVATE);
+
+        people.setChecked(preferences.getBoolean("sublist_people", false));
+        food.setChecked(preferences.getBoolean("sublist_food", false));
+        animals.setChecked(preferences.getBoolean("sublist_animals", false));
+        household.setChecked(preferences.getBoolean("sublist_household", false));
+        words.setChecked(preferences.getBoolean("sublist_words", false));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
